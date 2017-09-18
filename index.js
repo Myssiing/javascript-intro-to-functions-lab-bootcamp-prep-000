55d83b31de184c0e32a364e23b51cd130196b9e2
Tara Foulkrod <photochik02@hotmail.com>
2017-09-18 18:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,21 +28,26 @@ function logWhisper(spy) {
 
 function sayHiToGrandma(string) {
   console.log("hello")
-  console.log("HELLO")
-  console.log("I love you, Grandma.")
+  //console.log("HELLO")
+  //console.log("I love you, Grandma.")
 
   if("hello") {
     return "I can\'t hear you!"
-  } else if ("HELLO") {
-    return "YES INDEED!"
-  } else if ("I love you, Grandma.") {
-    return "I love you, too."
-  } else {
+} //else if ("HELLO") {
+//    return "YES INDEED!"
+//  } else if ("I love you, Grandma.") {
+//    return "I love you, too."
+//  } else {
 
-  }
+//  }
 
 }
 
+function sayHiToGrandma(string) {
+  console.log("HELLO")
+  if ("HELLO") {
+    return "YES INDEED!"
+  }
 /*function sayHiToGrandma(string) {
   if("HELLO") {
     return "YES INDEED!"
