2d9983249df530870f5c295155a38d4bbfc0a583
Tara Foulkrod <photochik02@hotmail.com>
2017-09-18 17:58:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,3 +31,16 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"
   }
 }
+
+function sayHiToGrandma(string) {
+  if("HELLO") {
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if("I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
